8a887e892a630fe13bf03a18f7085330502e95ec
Mihails Bodrins <m.bodrins@gmail.com>
2022-07-17 00:33:38 +0300

    выполнение задание на проверку допустимых значений в форме библиотекой pristine

---
+++ js/map.js
@@ -0,0 +1,8 @@
+// const renderPinsOnMap = (acomodationsData) => {
+//   const map = L.map
+//     .setView( {location});
+//   // 1.Создать (инициализировать) карту leaflet
+//   // 2.Создать шаблон пина
+//   // 3.Для каждого из объявлений acomodationsData рассставить пин на карте
+//   // 4. Добавлить красный пин
+// };
